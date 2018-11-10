eec365a876bfabb4bc3c3c3274d6eab61095a1d7
mikoto2000 <mikoto2000@gmail.com>
2018-11-10 17:47:30 +0900

    Axis 取得、 XYChart 取得のロジックを追加。

--- src/main/java/org/embulk/output/chart/ChartOutputPlugin.java
+++ src/main/java/org/embulk/output/chart/ChartOutputPlugin.java
@@ -2,6 +2,16 @@ package org.embulk.output.chart;
 
 import java.util.List;
 
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.Chart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
+
 import com.google.common.base.Optional;
 
 import org.embulk.config.Config;
@@ -75,5 +85,43 @@ public class ChartOutputPlugin
 
         // Write your code here :)
         throw new UnsupportedOperationException("ChartOutputPlugin.run method is not implemented yet");
+    }
+
+    enum ChartType {
+        BAR,
+        LINE,
+        SCATTER,
+        STACKED_BAR
+    }
+
+    enum AxisType {
+        NUMBER,
+        CATEGORY
+    }
+
+    private Axis getAxis(AxisType axisType) {
+        switch (axisType) {
+            case NUMBER:
+                return new NumberAxis();
+            case CATEGORY:
+                return new CategoryAxis();
+            default:
+                throw new RuntimeException("Unsupported Axis Type: " + axisType);
+        }
+    }
+
+    private <X,Y> XYChart getXYChart(ChartType chartType, Axis<X> xAxis, Axis<Y> yAxis) {
+        switch (chartType) {
+            case BAR:
+                return new BarChart<X, Y>(xAxis, yAxis);
+            case LINE:
+                return new LineChart<X, Y>(xAxis, yAxis);
+            case SCATTER:
+                return new ScatterChart<X, Y>(xAxis, yAxis);
+            case STACKED_BAR:
+                return new StackedBarChart<X, Y>(xAxis, yAxis);
+            default:
+                throw new RuntimeException("Unsupported Chart Type: " + chartType);
+        }
     }
 }
